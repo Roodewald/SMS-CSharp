4a558f09b8bde9217cef39e6d30b0ffc19e2a3ec
Pavel <pavel999977@gmail.com>
2021-04-29 11:23:24 +0500

    First commit

---
+++ GetInfo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Для работы c MainSMS необходимо подключить библиотеку mainsms
+using System.Net;
+using System.IO;
+
+namespace SmsApp
+{
+    namespace getinfo
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                string Host = Dns.GetHostName();
+                string IP = Dns.GetHostEntry(Host).AddressList[4].ToString();
+
+                string targetPhone;
+                int targetDirs;
+
+                string[] getFiles = Directory.GetDirectories(@"c:\");
+
+                string GetDirs(int howmanny)
+                {
+                    Array.Resize(ref getFiles, howmanny);
+                    return string.Join(", ", getFiles);
+                }
+
+                if (args.Length == 0)
+                {
+                    System.Console.WriteLine("Вы не ввели аргументы");
+                    Console.WriteLine("Введите номер через 7ку без плюса на который вы хотите отправить сообщение:");
+                    targetPhone = Console.ReadLine();
+                    Console.WriteLine("Cколько отправить директорий");
+                    targetDirs = Int32.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    targetPhone = args[0];
+                    targetDirs = Int32.Parse(args[1]);
+                }
+
+                Console.WriteLine(GetDirs(targetDirs) + " IP:" + IP);
+            }
+        }
+    }
+}
---
+++ SmsSends/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Для работы c MainSMS необходимо подключить библиотеку mainsms\
+using MainSms;
+using System.Net;
+using System.IO;
+
+namespace SmsApp
+{
+    namespace SmsSends
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string project_id = "SmsC";
+                string api_key = "6c3885b2426e330188a5a7c842cdf0d9";
+
+                string massage = "";
+                string targetPhone;
+                string a;
+
+                SmsMessage sms = new SmsMessage(project_id, api_key);
+
+                if (args.Length == 0)
+                {
+                    targetPhone = args[0];
+                    while ((a = Console.In.ReadLine()) !=null)
+                    {
+                        massage += a + ", ";
+                    }
+                    Console.WriteLine(massage);
+                }
+                else
+                {
+                    Console.WriteLine("Enter Arguments");
+                }
+
+                /*ResponseSend responseSend = sms.sendSms("sendertest", targetPhone, massage);
+                if (responseSend.status == "success")
+                {
+                    Console.WriteLine($"Частей в одной смс {responseSend.parts}, всего частей {responseSend.count}, стоимость отправки {responseSend.price}");
+                }
+                else
+                {
+                    Console.WriteLine("Error - " + responseSend.message);
+                }
+                ResponseBalance responseBalance = sms.getBalance();
+                if (responseBalance.status == "success") Console.WriteLine(responseBalance.balance);
+                else Console.WriteLine("Error - " + responseBalance.message);
+                Console.ReadKey();*/
+            }
+        }
+    }
+}
